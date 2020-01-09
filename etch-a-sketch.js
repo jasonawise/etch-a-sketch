7dd209cd8d54a1db47b59677af70dcb14c3f9621
Jason Wise <jasonandrewwise@gmail.com>
2020-01-08 20:55:21 -0500

    creating random starting point s

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,19 @@
-console.log('it worked');
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakebutton = document.querySelector('.shake');
+
+// Setup canvas for drawing
+const { width, height } = canvas;
+
+// create random x and y for starting point in canvas
+const x = Math.floor(Math.random() * width);
+const y = Math.floor(Math.random() * height);
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath();
+ctx.lineTo(x, y);
+ctx.moveTo(x, y);
+ctx.stroke();
